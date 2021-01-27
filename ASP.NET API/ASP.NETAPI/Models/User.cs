8a1921149ae5c8ccf4d755882ed8a8175b93c04f
Wizzorb <57352516+Wizzorb@users.noreply.github.com>
2021-01-27 11:39:10 +0000

    More renamings

---
+++ ASP.NET API/ASP.NETAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace APIcomp2001.Models
+{
+    public partial class User
+    {
+        [JsonIgnore]
+        public int UserId { get; set; }
+        public string UserFname { get; set; }
+        public string UserLname { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPword { get; set; }
+    }
+}
